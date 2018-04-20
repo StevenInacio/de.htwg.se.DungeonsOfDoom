e89f561cff3146af8c485a811956162d9e885a57
Steven Inácio <sthenriq@htwg-konstanz.de>
2018-04-20 11:05:18 +0200

    Made some changes, starting work

--- src/main/scala/de/htwg/se/DungeonsOfDoom/model/pawns/Spielfigur.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/model/pawns/Spielfigur.scala
@@ -2,8 +2,22 @@ package de.htwg.se.DungeonsOfDoom.model.pawns
 
 trait Spielfigur {
   val name : String
-  /*var HP : Integer
-  var Attack : Integer
-  var Defense : Integer*/
-  // whatever a character should have
+  var Körper : Integer
+  var Stärke : Integer
+  var Härte : Integer
+  var Agilität : Integer
+  var Bewegung : Integer
+  var Geschick : Integer
+  var Geist : Integer
+  var Verstand : Integer
+  var Aura : Integer
+  var Panzerungswert : Integer = 0
+  var Nahkampfbonus : Integer = 0
+  var Fernkampfbonus : Integer = 0
+  def Lebenskraft : Integer = {
+    Körper + Härte + 10
+  }
+  def Abwehr : Integer = {
+    Körper + Härte + Panzerungswert
+  }
 }
