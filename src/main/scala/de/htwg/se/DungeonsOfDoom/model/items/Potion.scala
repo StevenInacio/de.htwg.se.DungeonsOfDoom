fecac44a87e2b5b2d777a8196807e67641b97688
Christian Schmeißer <lambretta@live.de>
2018-05-27 19:57:03 +0200

    update item model

--- src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/Potion.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/Potion.scala
@@ -6,4 +6,6 @@ case class HealingPotion (name : String,
                           value : Integer,
                           val healthBonus : Integer
                          ) extends Item with Consumable {
+  //add random value between 1 and healthBonus to player health
+  //this.health += random(1, healthBonus) access player health??
 }
