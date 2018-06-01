30fddc66df3fc85e0209634905217b49769fc723
lordkeks <93Gcs666>
2018-06-01 10:54:16 +0200

    Omittable Standard Value for item usage added

--- src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/HealingPotion.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/HealingPotion.scala
@@ -3,6 +3,6 @@ package de.htwg.se.DungeonsOfDoom.model.items
 case class HealingPotion (name : String,
                           weight : Integer,
                           value : Integer,
-                          var usage : Integer,
+                          override var usage : Integer = 1,
                           healthBonus : Integer
                          ) extends Item with Consumable
