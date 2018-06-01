c26b8797afaec87374bf56cbfb56a05b66d20715
lordkeks <93Gcs666>
2018-06-01 10:38:13 +0200

    Healthspec coverage is weird

--- src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/HealingPotion.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/model/items/HealingPotion.scala
@@ -5,11 +5,4 @@ case class HealingPotion (name : String,
                           value : Integer,
                           var usage : Integer,
                           healthBonus : Integer
-                         ) extends Item with Consumable {
-  //add random value between 1 and healthBonus to player health
-  // Do this in the controller layer
-  // player changeHealth random(1,HealingPotion.healthBonus)
-
-  // Event Listener ist in Controller
-  // View schickt Events an EventListener (Controller)
-}
+                         ) extends Item with Consumable
