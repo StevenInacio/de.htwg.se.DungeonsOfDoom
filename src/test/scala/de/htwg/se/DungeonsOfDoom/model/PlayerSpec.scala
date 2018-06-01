2e787a148a3e41e59dddd43ecf8600807be8b03f
lordkeks <93Gcs666>
2018-06-01 08:56:14 +0200

    modified PlayerSpec

--- src/test/scala/de/htwg/se/DungeonsOfDoom/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/DungeonsOfDoom/model/PlayerSpec.scala
@@ -12,11 +12,25 @@ class PlayerSpec extends WordSpec with Matchers {
       val player = Player("Your Name", 0, 0, 0, 0, 0, 0, 0, 0, 0)
       "have a name" in {
         player.name should be("Your Name")
+        player.body should be(0)
+        player.strength should be(0)
+        player.hardness should be(0)
+        player.agility should be(0)
+        player.mobility should be(0)
+        player.dexterity should be(0)
+        player.spirit should be(0)
+        player.mind should be(0)
+        player.aura should be(0)
       }
       "have a nice String representation" in {
         player.toString should be("Your Name")
       }
+      "be able to equip items" in {
 
+      }
+      "be able to unequip items" in {
+
+      }
     }
   }
 }
