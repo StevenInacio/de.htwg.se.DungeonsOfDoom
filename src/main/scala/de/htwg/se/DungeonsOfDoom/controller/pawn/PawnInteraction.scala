ca35815434537680b9bf3d29c877a896682c264b
lordkeks <93Gcs666>
2018-06-06 16:54:33 +0200

    transfered PawnInteraction and Dice from old Branch (sorry, my bad..)

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/pawn/PawnInteraction.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/pawn/PawnInteraction.scala
@@ -1,5 +1,27 @@
 package de.htwg.se.DungeonsOfDoom.controller.pawn
 
-object PawnInteraction {
+import de.htwg.se.DungeonsOfDoom.controller.utility.Dice
+import de.htwg.se.DungeonsOfDoom.model.pawns.Pawn
 
+object PawnInteraction {
+  def attack(attacker: Pawn, target: Pawn): Unit = {
+    var result = Dice.roll(20)
+    if (result < attacker.hit && result < 20) {
+      val defend = Dice.roll(20)
+      if (defend <= target.defense && defend < 20) {
+        result -= defend
+      }
+      if (result < 0) {
+        result = 0
+      }
+      target.changeHealth(-result)
+    }
+  }
+
+  /*def heal(healer: Pawn, target: Pawn): Unit = {
+    var result = Dice.roll(20)
+    if (result < healer.castSpell && result < 20){
+      target.changeHealth(result)
+    }
+  }*/
 }
--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/Dice.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/Dice.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.DungeonsOfDoom.controller.utility
 
 object Dice {
-
+  def roll(size: Integer): Integer = {
+    val r = scala.util.Random
+    r.nextInt(size) + 1
+  }
 }
