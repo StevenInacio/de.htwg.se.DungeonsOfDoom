cf514ceeb18919c6fd742b4e78b2d91ee8a4d6d6
lordkeks <93Gcs666>
2018-06-08 08:31:01 +0200

    done with DiceSpec

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/Dice.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/Dice.scala
@@ -2,7 +2,9 @@ package de.htwg.se.DungeonsOfDoom.controller.utility
 
 object Dice {
   def roll(size: Integer): Integer = {
-    val r = scala.util.Random
-    r.nextInt(size) + 1
+    if (size > 0) {
+      val r = scala.util.Random
+      r.nextInt(size) + 1
+    } else 0
   }
 }
