74a7efc0a0e0f8305d6aa1e3143e29826ab18521
lordkeks <93Gcs666>
2018-06-08 11:24:30 +0200

    ItemInteractionSpec init

--- src/test/scala/de/htwg/se/DungeonsOfDoom/controller/ItemInteractionSpec.scala
+++ src/test/scala/de/htwg/se/DungeonsOfDoom/controller/ItemInteractionSpec.scala
@@ -1,5 +1,22 @@
 package de.htwg.se.DungeonsOfDoom.controller
 
-class ItemInteractionSpec {
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+
+@RunWith(classOf[JUnitRunner])
+class ItemInteractionSpec {
+  //TODO create item weapon
+  //TODO create item healingPotion with multiple usages
+  //TODO create player (with both items in inventory
+  //TODO create floor
+
+  //TODO drop any item
+  //TODO pickup that item
+  //TODO equip weapon
+  //TODO unequip weapon
+  //TODO use healingPotion
+  //TODO use healingPotion again
+  //TODO drop healingPotion
+  //TODO try using healing Potion
 }
