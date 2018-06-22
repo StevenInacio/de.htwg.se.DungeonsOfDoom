657de977a48815790d92444da5931248bb44895e
Christian Schmeisser <lambretta@live.de>
2018-06-22 11:42:45 +0200

    Commented getState, saveState, undo and loadState. Get the game running first, then implement undo!

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
@@ -3,21 +3,21 @@ import java.io.File
 import de.htwg.se.DungeonsOfDoom.model.board.Field
 
 object TimeManager {
-  def getState(map: Array[Array[Field]]) : State = {
+  //def getState(map: Array[Array[Field]]) : State = {
     // TODO
-    new State()
-  }
-  def saveState(state: State) : Unit = {
-    // TODO
-  }
+  //  new State()
+  //}
+  //def saveState(state: State) : Unit = {
+  //  // TODO
+  //}
   def advanceTime() : Unit = {
     // TODO
   }
-  def undo() : Unit = {
+  //def undo() : Unit = {
     // TODO
-  }
-  def loadState(xml: File) : State = {
+  //}
+  //def loadState(xml: File) : State = {
     // TODO
-    new State()
-  }
+  //  new State()
+  //}
 }
