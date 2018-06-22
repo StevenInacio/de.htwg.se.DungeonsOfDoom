6f12e9bf7762d2f0f23a2aad2c4176b14786542c
Christian Schmeisser <lambretta@live.de>
2018-06-22 11:33:58 +0200

    TimeManager State init

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
@@ -1,5 +1,23 @@
 package de.htwg.se.DungeonsOfDoom.controller.utility
+import java.io.File
+import de.htwg.se.DungeonsOfDoom.model.board.Field
 
 object TimeManager {
-
+  def getState(map: Array[Array[Field]]) : State = {
+    // TODO
+    new State()
+  }
+  def saveState(state: State) : Unit = {
+    // TODO
+  }
+  def advanceTime() : Unit = {
+    // TODO
+  }
+  def undo() : Unit = {
+    // TODO
+  }
+  def loadState(xml: File) : State = {
+    // TODO
+    new State()
+  }
 }
