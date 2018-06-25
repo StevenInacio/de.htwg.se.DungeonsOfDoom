381087e318cbde2511df07214f5079548c24e6f2
Christian Schmeißer <lambretta@live.de>
2018-06-25 21:12:58 +0200

    update time manager (the f am i supposed to do!?)

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
@@ -3,21 +3,22 @@ import java.io.File
 import de.htwg.se.DungeonsOfDoom.model.board.Field
 
 object TimeManager {
+  var run = true;
   //def getState(map: Array[Array[Field]]) : State = {
-    // TODO
-  //  new State()
+    // _TODO
   //}
   //def saveState(state: State) : Unit = {
-  //  // TODO
+    // _TODO
   //}
   def advanceTime() : Unit = {
-    // TODO
+    while(run) {
+
+    }
   }
   //def undo() : Unit = {
-    // TODO
+    // _TODO
   //}
   //def loadState(xml: File) : State = {
-    // TODO
-  //  new State()
+    // _TODO
   //}
 }
