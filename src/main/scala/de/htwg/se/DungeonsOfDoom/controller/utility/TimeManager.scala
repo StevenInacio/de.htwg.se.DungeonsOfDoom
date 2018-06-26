f4942f18ae07c7eef38b7685301c507ccccc5818
Christian Schmeisser <lambretta@live.de>
2018-06-26 19:03:20 +0200

    TimeManager blablubb

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
@@ -9,13 +9,32 @@ object TimeManager {
   //def saveState(state: State) : Unit = {
     // _TODO
   //}
-  def advanceTime() : Unit = {
-
-  }
+  //def advanceTime() : Unit = {
+  //}
   //def undo() : Unit = {
     // _TODO
   //}
   //def loadState(xml: File) : State = {
     // _TODO
   //}
+
+  //DEBUG --- this belongs to the UI
+    def startGame() : Unit = {
+      while(playerTurn("attack") != "quit"){
+        enemyTurn()
+      }
+    }
+  // returns false if the user wants to exit the game
+  def playerTurn(command : String) : String = {
+    var iter = command.split(' ')
+    for(i <- iter){
+
+    }
+
+    true//DEBUG
+    }
+
+  def enemyTurn() : Unit = {
+    //TODO enemy does evil things
+  }
 }
