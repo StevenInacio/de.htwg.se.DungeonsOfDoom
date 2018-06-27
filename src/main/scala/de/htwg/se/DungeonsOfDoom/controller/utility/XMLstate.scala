9573ea4a5f5b2d6e102cf1ce6675c59cbacc6c45
Christian Schmeisser <lambretta@live.de>
2018-06-27 13:45:04 +0200

    toXML refactor (added equippedToXML and inventoryToXML)

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
@@ -2,6 +2,9 @@ package de.htwg.se.DungeonsOfDoom.controller.utility
 
 import de.htwg.se.DungeonsOfDoom.controller.board.BoardInteraction
 import de.htwg.se.DungeonsOfDoom.model.items.{HealingPotion, Weapon}
+import de.htwg.se.DungeonsOfDoom.model.pawns.Pawn
+
+import scala.xml.Node
 
 
 class XMLstate {
@@ -20,43 +23,8 @@ class XMLstate {
         <aura>{BoardInteraction.player.aura}</aura>
         <positionx>{BoardInteraction.player.currentPosition._1}</positionx>
         <positiony>{BoardInteraction.player.currentPosition._2}</positiony>
-        <inventory>{
-          for(item <- BoardInteraction.player.inventory){
-            item match {
-              case w : Weapon => <weapon>
-                                    <name>{w.name}</name>
-                                    <durability>{w.durability}</durability>
-                                    <maxdurability>{w.maxDurability}</maxdurability>
-                                    <weight>{w.weight}</weight>
-                                    <value>{w.value}</value>
-                                    <damage>{w.damage}</damage>
-                                    <minstrength>{w.minStrength}</minstrength>
-                                  </weapon>
-              case h : HealingPotion => <healingpotion>
-                                          <name>{h.name}</name>
-                                          <weight>{h.weight}</weight>
-                                          <value>{h.value}</value>
-                                          <usage>{h.usage}</usage>
-                                          <healthbonus>{h.healthBonus}</healthbonus>
-                                        </healingpotion>
-            }
-          }
-        }</inventory>
-        <equipped>{
-          for(item <- BoardInteraction.player.equipped){
-            item match {
-              case w : Weapon => <weapon>
-                                  <name>{w.name}</name>
-                                  <durability>{w.durability}</durability>
-                                  <maxdurability>{w.maxDurability}</maxdurability>
-                                  <weight>{w.weight}</weight>
-                                  <value>{w.value}</value>
-                                  <damage>{w.damage}</damage>
-                                  <minstrength>{w.minStrength}</minstrength>
-                                </weapon>
-            }
-          }
-        }</equipped>
+        inventoryToXML(BoardInteraction.player)
+        equippedToXML(BoardInteraction.player)
       </player>
       <enemys>{
         for (enemy <- BoardInteraction.enemyList) {
@@ -73,53 +41,58 @@ class XMLstate {
             <aura>{enemy.aura}</aura>
             <positionx>{enemy.currentPosition._1}</positionx>
             <positiony>{enemy.currentPosition._2}</positiony>
-
-            <inventory>{
-              for(item <- enemy.inventory){
-                item match {
-                  case w : Weapon => <weapon>
-                                        <name>{w.name}</name>
-                                        <durability>{w.durability}</durability>
-                                        <maxdurability>{w.maxDurability}</maxdurability>
-                                        <weight>{w.weight}</weight>
-                                        <value>{w.value}</value>
-                                        <damage>{w.damage}</damage>
-                                        <minstrength>{w.minStrength}</minstrength>
-                                      </weapon>
-                  case h : HealingPotion => <healingpotion>
-                                              <name>{h.name}</name>
-                                              <weight>{h.weight}</weight>
-                                              <value>{h.value}</value>
-                                              <usage>{h.usage}</usage>
-                                              <healthbonus>{h.healthBonus}</healthbonus>
-                                            </healingpotion>
-                }
-              }
-              }</inventory>
-            <equipped>{
-              for(item <- enemy.equipped){
-                item match {
-                  case w : Weapon => <weapon>
-                                        <name>{w.name}</name>
-                                        <durability>{w.durability}</durability>
-                                        <maxdurability>{w.maxDurability}</maxdurability>
-                                        <weight>{w.weight}</weight>
-                                        <value>{w.value}</value>
-                                        <damage>{w.damage}</damage>
-                                        <minstrength>{w.minStrength}</minstrength>
-                                      </weapon>
-                }
-              }
-              }</equipped>
-
+            inventoryToXML(enemy)
+            equippedToXML(enemy)
           </enemy>
         }
       }
       </enemys>
-
-    //TODO save each field
-    //TODO save each fields items
-
+      <board>{
+        //TODO save each field
+        //TODO save each fields items
+        }</board>
     </state>
+  }
+
+  def equippedToXML(pawn: Pawn) : Node = {
+    <equipped>{
+      for(item <- pawn.equipped){
+        item match {
+          case w : Weapon => <weapon>
+            <name>{w.name}</name>
+            <durability>{w.durability}</durability>
+            <maxdurability>{w.maxDurability}</maxdurability>
+            <weight>{w.weight}</weight>
+            <value>{w.value}</value>
+            <damage>{w.damage}</damage>
+            <minstrength>{w.minStrength}</minstrength>
+          </weapon>
+        }
+      }
+    }</equipped>
+  }
+  def inventoryToXML(pawn: Pawn) : Node = {
+    <inventory>{
+      for(item <- pawn.inventory){
+        item match {
+          case w : Weapon => <weapon>
+            <name>{w.name}</name>
+            <durability>{w.durability}</durability>
+            <maxdurability>{w.maxDurability}</maxdurability>
+            <weight>{w.weight}</weight>
+            <value>{w.value}</value>
+            <damage>{w.damage}</damage>
+            <minstrength>{w.minStrength}</minstrength>
+          </weapon>
+          case h : HealingPotion => <healingpotion>
+            <name>{h.name}</name>
+            <weight>{h.weight}</weight>
+            <value>{h.value}</value>
+            <usage>{h.usage}</usage>
+            <healthbonus>{h.healthBonus}</healthbonus>
+          </healingpotion>
+        }
+      }
+    }</inventory>
   }
 }
