b608f99613e75b45c023a503ea48e7b5c6bf3b7b
Christian Schmeisser <lambretta@live.de>
2018-06-27 13:02:29 +0200

    toXML done player save

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
@@ -1,17 +1,47 @@
 package de.htwg.se.DungeonsOfDoom.controller.utility
 
 import de.htwg.se.DungeonsOfDoom.controller.board.BoardInteraction
+import de.htwg.se.DungeonsOfDoom.model.items.{HealingPotion, Weapon}
 
 
 class XMLstate {
   def toXML() : Unit = {
     <state>
       <player>
-        <position>
-          {BoardInteraction.player.currentPosition}
-        </position>
+        <name>{BoardInteraction.player.name}</name>
+        <body>{BoardInteraction.player.body}</body>
+        <strength>{BoardInteraction.player.strength}</strength>
+        <hardness>{BoardInteraction.player.hardness}</hardness>
+        <agility>{BoardInteraction.player.agility}</agility>
+        <mobility>{BoardInteraction.player.mobility}</mobility>
+        <dexterity>{BoardInteraction.player.dexterity}</dexterity>
+        <spirit>{BoardInteraction.player.spirit}</spirit>
+        <mind>{BoardInteraction.player.mind}</mind>
+        <aura>{BoardInteraction.player.aura}</aura>
+        <position>{BoardInteraction.player.currentPosition}</position>
+        <inventory>{
+          for(item <- BoardInteraction.player.inventory){
+            item match {
+              case w : Weapon => <weapon>
+                                    <name>{w.name}</name>
+                                    <durability>{w.durability}</durability>
+                                    <maxdurability>{w.maxDurability}</maxdurability>
+                                    <weight>{w.weight}</weight>
+                                    <value>{w.value}</value>
+                                    <damage>{w.damage}</damage>
+                                    <minstrength>{w.minStrength}</minstrength>
+                                  </weapon>
+              case h : HealingPotion => <healingpotion>
+                                          <name>{h.name}</name>
+                                          <weight>{h.weight}</weight>
+                                          <value>{h.value}</value>
+                                          <usage>{h.usage}</usage>
+                                          <healthbonus>{h.healthBonus}</healthbonus>
+                                        </healingpotion>
+            }
+          }
+        }</inventory>
       </player>
-    //TODO save players items
     //TODO save each enemy (BoardInteraction.enemyList)
     //TODO save each enemys items
     //TODO save each field
