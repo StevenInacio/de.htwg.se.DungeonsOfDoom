745f89e641bcf80e90427186137f91d4c8f25d8d
Christian Schmeisser <lambretta@live.de>
2018-06-27 11:54:12 +0200

    toXML init

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/XMLstate.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.DungeonsOfDoom.controller.utility
 
-trait XMLstate {
-
+class XMLstate {
+  def toXML() : Unit = {
+    //TODO save player
+    //TODO save players items
+    //TODO save each enemy
+    //TODO save each enemys items
+    //TODO save each field
+    //TODO save each fields items
+  }
 }
