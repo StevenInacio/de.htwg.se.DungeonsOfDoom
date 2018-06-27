b1c31283cf9b0e97d3b0e38b954935b637e5cc85
Christian Schmeisser <lambretta@live.de>
2018-06-27 10:18:54 +0200

    added command structure

--- src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/controller/utility/TimeManager.scala
@@ -3,36 +3,10 @@ package de.htwg.se.DungeonsOfDoom.controller.utility
 import de.htwg.se.DungeonsOfDoom.model.board.Field
 
 object TimeManager {
-  //def getState(map: Array[Array[Field]]) : State = {
-    // _TODO
-  //}
-  //def saveState(state: State) : Unit = {
-    // _TODO
-  //}
-  //def advanceTime() : Unit = {
-  //}
-  //def undo() : Unit = {
-    // _TODO
-  //}
-  //def loadState(xml: File) : State = {
-    // _TODO
-  //}
-
-  //DEBUG --- this belongs to the UI
-    def startGame() : Unit = {
-      while(playerTurn("attack") != "quit"){
-        enemyTurn()
-      }
-    }
   // returns false if the user wants to exit the game
-  def playerTurn(command : String) : String = {
-    var iter = command.split(' ')
-    for(i <- iter){
-
-    }
-
-    true//DEBUG
-    }
+  def playerTurn() : String = {
+    "DEBUG"
+  }
 
   def enemyTurn() : Unit = {
     //TODO enemy does evil things
