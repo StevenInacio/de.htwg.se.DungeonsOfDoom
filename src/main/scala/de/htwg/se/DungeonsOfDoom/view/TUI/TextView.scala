fbd3dfdd6763ebd7d41b6aa0f25b6da479d457b9
Christian Schmeisser <lambretta@live.de>
2018-06-28 15:47:38 +0200

    added debug channels to support different output

--- src/main/scala/de/htwg/se/DungeonsOfDoom/view/TUI/TextView.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/view/TUI/TextView.scala
@@ -4,10 +4,15 @@ class TextView {
   val DEBUG = true
   val ERROR = true
   val OUT = true
-  def log(channel : Boolean, input : String) : Unit = {
-    if(channel) print(input)
+  def logDebug(input : String) : Unit = {
+    if(DEBUG) print(input)
   }
-
+  def logError(input : String) : Unit = {
+    if(ERROR) print(input)
+  }
+  def logOut(input : String) : Unit = {
+    if(OUT) print(input)
+  }
 }
 
 
