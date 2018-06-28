509e96b71ae2ed8b15f62021a3ee33265374555e
Christian Schmeisser <lambretta@live.de>
2018-06-28 14:14:45 +0200

    logger init

--- src/main/scala/de/htwg/se/DungeonsOfDoom/view/TUI/TextView.scala
+++ src/main/scala/de/htwg/se/DungeonsOfDoom/view/TUI/TextView.scala
@@ -1,5 +1,13 @@
 package de.htwg.se.DungeonsOfDoom.view.TUI
 
 class TextView {
+  val DEBUG = true
+  val ERROR = true
+  val OUT = true
+  def log(channel : Boolean, input : String) : Unit = {
+    if(channel) print(input)
+  }
 
 }
+
+
